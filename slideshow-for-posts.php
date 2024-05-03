845c758817c3fe9403267a9e8a4b15b5a0d6722f
baberparweez <baberparweez@gmail.com>
2024-05-03 09:59:48 -0300

    rename author

--- slideshow-for-posts.php
+++ slideshow-for-posts.php
@@ -6,7 +6,7 @@
  * Requires at least: 6.1
  * Requires PHP:      7.0
  * Version:           0.1.0
- * Author:            The WordPress Contributors
+ * Author:            Baber Parweez
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       slideshow-for-posts
@@ -15,7 +15,7 @@
  */
 
 if (!defined('ABSPATH')) {
-    exit; // Exit if accessed directly.
+	exit; // Exit if accessed directly.
 }
 
 /**
@@ -27,6 +27,6 @@ if (!defined('ABSPATH')) {
  */
 function slideshow_for_posts_slideshow_for_posts_block_init()
 {
-    register_block_type(__DIR__ . '/build');
+	register_block_type(__DIR__ . '/build');
 }
 add_action('init', 'slideshow_for_posts_slideshow_for_posts_block_init');
